0dec73c103aa49d517fe29422e967fbfbdd72db0
Manuel1977 <Manuel_fe1977@hotmail.com>
2022-12-11 18:09:07 +0100

    login, inicio registro, modificacion de header y footer

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Validators, FormBuilder } from "@angular/forms";
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
@@ -7,9 +7,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  submitted: boolean = false;
+
+  formRegistro = this.fb.group(
+    {
+      name: ['', Validators.required],
+      apellidos: ['', Validators.required],
+      telefono: ['', Validators.required],
+      password: ['', Validators.required],
+      password2: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      especialidad: ['', Validators.required],
+      privacidad: [false, Validators.requiredTrue],
+    }
+  );
+  public get password(): boolean {
+    return this.formRegistro.get('password')?.invalid || false;
+  }
+  public get password2(): boolean {
+    return this.formRegistro.get('password2')?.invalid || false;
+  }
+  public get name(): boolean {
+    return this.formRegistro.get('name')?.invalid || false;
+  }
+  public get apellidos(): boolean {
+    return this.formRegistro.get('apellidos')?.invalid || false;
+  }
+  public get email(): boolean {
+    return this.formRegistro.get('email')?.invalid || false;
+  }
+  public get telefono(): boolean {
+    return this.formRegistro.get('telefono')?.invalid || false;
+  }
+  public get especialidad(): boolean {
+    return this.formRegistro.get('especialidad')?.invalid || false;
+  }
+  public get privacidad(): boolean {
+    return this.formRegistro.get('privacidad')?.invalid || false;
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  onRegister() {
+
+    this.submitted = true;
+    if (!this.formRegistro.valid) {
+      return;
+    }
+
+    //TODO event emiter con formContacto
+    console.log(this.formRegistro.value, this.formRegistro.valid);
+    this.formRegistro.reset();
+    this.submitted = false;
+  }
 }
