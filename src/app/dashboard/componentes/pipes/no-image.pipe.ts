a378812af4b3a042bd548533aa3677283e26be9c
Manuel1977 <Manuel_fe1977@hotmail.com>
2023-02-22 21:30:03 +0100

    unsafe URL

--- src/app/dashboard/componentes/pipes/no-image.pipe.ts
+++ src/app/dashboard/componentes/pipes/no-image.pipe.ts
@@ -1,17 +1,23 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Pipe({
   name: 'noImage'
 })
 export class NoImagePipe implements PipeTransform {
 
-  transform(image:string): string {
+  constructor(private sanitizer:DomSanitizer){}
+
+  transform(image:string): any {
     let resultado=image;
+
+
     
     if (image===''||image===null || image===undefined){      
       resultado= './../../../../assets/images/no_disponible.png';
     }    
-    return resultado
+    return this.sanitizer.bypassSecurityTrustResourceUrl(resultado);
+
   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@
 export const environment = {
   production: true,
   mapBoxToken:'pk.eyJ1IjoibWFudWVsbzE5NzciLCJhIjoiY2xjMHkwa2JqM2IzdzN2cW5mMnl4aWVzYSJ9.7t0I5taB1LvdoXHYbL5fqw',
-  base_url:'http://localhost:8000/api',
+  base_url:'https://guarded-profit-production.up.railway.app/api',
   especialistaAdmin:'06e4d7bf-3e89-4936-bcbe-194f2568d853'
 };
