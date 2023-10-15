606ddca53512d2fd6a9c5f630fd57e00ecf8ce39
Manuel1977 <Manuel_fe1977@hotmail.com>
2023-10-15 13:49:12 +0200

    cambios enviromment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,8 +4,9 @@
 export const environment = {
   production: true,
   mapBoxToken:'pk.eyJ1IjoibWFudWVsbzE5NzciLCJhIjoiY2xjMHkwa2JqM2IzdzN2cW5mMnl4aWVzYSJ9.7t0I5taB1LvdoXHYbL5fqw',
-  base_url:'https://nativos-tierra-app-6d5iw.ondigitalocean.app/api',
+  base_url:'https://dolphin-app-zu6mr.ondigitalocean.app/api',
+  
   especialistaAdmin:'06e4d7bf-3e89-4936-bcbe-194f2568d853',
-  url_socket:'https://nativos-tierra-app-6d5iw.ondigitalocean.app/',
+  url_socket:'https://dolphin-app-zu6mr.ondigitalocean.app',
   
 };
